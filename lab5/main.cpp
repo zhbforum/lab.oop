9c356b53deed1b217b194f8405a91fd750d36ad1
zhbforum <69122344+zhbforum@users.noreply.github.com>
2024-05-17 23:26:38 +0000

    Lab5

---
+++ lab5/main.cpp
@@ -0,0 +1,28 @@
+#include "lab5/vector.h"
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    Vector v1(3, 4), v2(1, 4);
+    Vector v3;
+    v3 = v1;
+    cout << "Vector v1: ";
+    v1.print();
+    cout << "Vector v3 after assignment: ";
+    v3.print();
+
+    --v1;
+    cout << "Vector v1 after prefix decrement: ";
+    v1.print();
+
+    v1--;
+    cout << "Vector v1 after postfix decrement: ";
+    v1.print();
+
+    double angle = v1 / v2;
+    cout << "Angle between v1 and v2: " << angle << " degrees" << endl;
+
+    return 0;
+}
