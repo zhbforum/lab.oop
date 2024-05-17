cced3a217da413c0b2d3111671f4ebcea9025741
zhbforum <69122344+zhbforum@users.noreply.github.com>
2024-05-17 23:36:01 +0000

    Final version of lab4

--- lab4/lab4_var14.cpp
+++ lab4/lab4_var14.cpp
@@ -1,59 +1,7 @@
-#include <iostream>
-#include <cmath>
+#include <iostream> 
+#include "lab4/vector.h" 
 
-using namespace std;
-
-class Vector 
-{
-private:
-    double x, y;
-
-public:
-    Vector(double x = 0.0, double y = 0.0) : x(x), y(y) {}
-
-    Vector& operator=(const Vector& other) 
-    {
-        if (this != &other) 
-        {
-            x = other.x;
-            y = other.y;
-        }
-        return *this;
-    }
-
-    Vector& operator--() 
-    {
-        x--;
-        y--;
-        return *this;
-    }
-
-    friend Vector operator--(Vector& v, int);
-
-    void print() const 
-    {
-        cout << "(" << x << ", " << y << ")" << endl;
-    }
-
-    friend double operator/(const Vector& v1, const Vector& v2);
-};
-
-Vector operator--(Vector& v, int) 
-{
-    Vector temp = v;
-    v.x--;
-    v.y--;
-    return temp;
-}
-
-double operator/(const Vector& v1, const Vector& v2) 
-{
-    double dotProduct = v1.x * v2.x + v1.y * v2.y;
-    double magnitudeV1 = sqrt(v1.x * v1.x + v1.y * v1.y);
-    double magnitudeV2 = sqrt(v2.x * v2.x + v2.y * v2.y);
-    double angle = acos(dotProduct / (magnitudeV1 * magnitudeV2));
-    return angle * (180.0 / M_PI); 
-}
+using namespace std; 
 
 int main() 
 {
